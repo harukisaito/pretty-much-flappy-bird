36cff4d866cac468e1cd81f3dd0f023e58d631dd
Haruki Saito <haruki.saito@artcom.de>
2019-01-11 11:16:16 +0100

    flappy bird

---
+++ Assets/Scripts/ScrollingObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingObject : MonoBehaviour {
+
+	private Rigidbody2D rigidbody2D;
+	// Use this for initialization
+	void Start () {
+		rigidbody2D = GetComponent<Rigidbody2D>();
+		rigidbody2D.velocity = new Vector2(GameControl.instance.scrollSpeed, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(GameControl.instance.gameOver == true)
+		{
+			rigidbody2D.velocity = Vector2.zero;
+		}
+	}
+}
